1de5abd74a9eb926bbba0e11fb2ede7dc3093fcd
JavierVillarAsensio <villarasensiojavier@gmail.com>
2024-05-29 19:24:10 +0200

    commit

--- hiperespectral.cpp
+++ hiperespectral.cpp
@@ -20,9 +20,17 @@
 #define IMG_PATH "jasperRidge2_R198/jasperRidge2_R198.img"
 #define HDR_PATH "jasperRidge2_R198/jasperRidge2_R198.hdr"
 
+#define ROAD_PATH "spectrums/manmade.concrete.pavingconcrete.solid.all.0092uuu_cnc.jhu.becknic.spectrum.txt"
+#define SOIL1_PATH "spectrums/soil.mollisol.cryoboroll.none.all.85p4663.jhu.becknic.spectrum.txt"
+#define SOIL2_PATH "spectrums/soil.utisol.hapludult.none.all.87p707.jhu.becknic.spectrum.txt"
+#define TREE_PATH "spectrums/vegetation.tree.eucalyptus.maculata.vswir.jpl087.jpl.asd.spectrum.txt"
+#define WATER_PATH "spectrums/water.tapwater.none.liquid.all.tapwater.jhu.becknic.spectrum.txt"
+
 #define WAVELENGTH_FIELD "wavelength"
 #define END_FIELD "}"
-#define CHANNELS_PER_LINE 6
+
+#define SPECTRUM_FIRST_VALUE_FIELD "First"
+#define SPECTRUM_LAST_VALUE_FIELD "Last" 
 
 using namespace std;
 
@@ -36,12 +44,12 @@ int read_img(float *img) {
     }
 
     int index = 0;
-    char buffer[2];
+    char buffer[DATA_SIZE];
     short int value;
     while(index < n_pixels){
-        file.read(buffer, 2);
+        file.read(buffer, DATA_SIZE);
 
-        memcpy(&value, buffer, 2);
+        memcpy(&value, buffer, DATA_SIZE);
         img[index] = static_cast<float>(value);
         index++;
     }
@@ -92,6 +100,28 @@ int read_hdr(float *channels){
 
 int main(){
 
+    ifstream file(WATER_PATH);
+    if(!file.is_open()){
+        cout << "Error opening the file, it could not be opened. Aborting." << endl;
+        return(EXIT_FAILURE);
+    }
+
+    string line;
+    string first;
+    int index = 0;
+    while (index < 25) {
+        getline(file, line);
+        istringstream line_stream(line);
+        line_stream >> first;
+        if (first == SPECTRUM_FIRST_VALUE_FIELD){
+            while(line_stream >> first);
+            cout << "Value: ";
+        }
+        cout << first << endl;
+        index++;
+    }
+
+    /*
     float *channels = (float*)malloc(CHANNELS * sizeof(float));
     if (read_hdr(channels) == EXIT_FAILURE)
         return EXIT_FAILURE;    
@@ -99,6 +129,6 @@ int main(){
     float *image = (float*)malloc(n_pixels * sizeof(float));
     if (read_img(image) == EXIT_FAILURE)
         return EXIT_FAILURE;
-
+    */
     return(EXIT_SUCCESS);
 }
