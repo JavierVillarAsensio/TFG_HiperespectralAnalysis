87c640e6c816133981182d2d684670788e5e768f
JavierVillarAsensio <villarasensiojavier@gmail.com>
2024-10-27 23:12:28 +0100

    finalizando perf swarm

--- performance/swarm_perf.sh
+++ performance/swarm_perf.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 file=swarm_times.txt
 file_path=performance/
+result=output/result.jpg
 rm -f $file
 
-save=save.yaml
 swarm_yaml=docker-swarm.yaml
 
-cp ../$swarm_yaml ../$save
-
 new_specs_folder=(1 2 3 4)
-old_n_specs=4
 
 echo "Performance with 4 spectrums (proposed result) with docker swarm:" > $file
 pushd ..
@@ -21,11 +18,19 @@ for sf in "${new_specs_folder[@]}"; do
     n_specs=$((sf * 4 + 4))
     echo -e "\nPerformance with $n_specs spectrums with docker swarm:" >> $file
     cp ../perf_specs/$sf/* ../spectrums
+
     pushd ..
-    sed -i "s/$old_n_specs/$n_specs/g" $swarm_yaml
-    { time ./run_swarm.sh; } 2>> $file_path$file
+    ( time (
+    docker service update --env-add NUM_FILES=$n_specs --env-rm NUM_FILES master;
+    docker service scale tfg-hiperespectral-analysis_replica=$n_specs;
+    docker service update --force tfg-hiperespectral-analysis_replica;
+    docker service update --force tfg-hiperespectral-analysis_master;
+    until [ -f "$result" ]; do
+        sleep 1
+    done
+    )) 2>> $file_path$file
     popd
-    old_n_specs=$n_specs
+
     make clean -C .. > /dev/null
 done
 
@@ -34,7 +39,5 @@ sed -i '/Network/d' $file
 sed -i '/sys/d' $file
 sed -i '/user/d' $file
 rm ../spectrums/meteorites*
-rm ../$swarm_yaml
-mv ../$save ../$swarm_yaml
 
 grep "real" $file > ../charts/swarm_times.txt
--- run_swarm.sh
+++ run_swarm.sh
@@ -2,19 +2,14 @@
 
 result=output/result.jpg
 stack_name=tfg-hiperespectral-analysis
-image=hiperespectral-replica-swarm:latest
-image_dockerfile=Docker_images/Dockerfile_replicas_swarm
+image_replica=hiperespectral-replica-swarm:latest
+image_replica_dockerfile=Docker_images/Dockerfile_replicas_swarm
 
-#count spectrums for docker replicas
-FOLDER="spectrums"
-NUM_FILES=$(find "$FOLDER" -type f | wc -l)
-echo -e "NUM_FILES=$NUM_FILES" > specs.env
-
-if docker image inspect ${image} > /dev/null 2>&1; then
+if docker image inspect ${image_replica} > /dev/null 2>&1; then
     echo "Image already built."
 else
-    echo "Building image: ${image}..."
-    docker build -t ${image} -f $image_dockerfile .
+    echo "Building image: ${image_replica}..."
+    docker build -t ${image_replica} -f $image_dockerfile .
 fi
 
 docker swarm init > /dev/null 2>&1
@@ -24,5 +19,3 @@ docker stack deploy -c docker-swarm.yaml $stack_name
 until [ -f $result ]; do
     sleep 1
 done
-
-docker stack rm $stack_name
